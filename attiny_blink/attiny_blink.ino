971c01d86af0b7deb808f9ba9059b9481e5dbe12
Oscar Bernal <39655326+ozcodex@users.noreply.github.com>
2021-06-21 15:08:04 -0500

    Update attiny_blink.ino

--- attiny_blink/attiny_blink.ino
+++ attiny_blink/attiny_blink.ino
@@ -4,11 +4,13 @@ const int sleep = 1000;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
-  bitSet(DDRB, led);
+  pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever
 void loop() {
-  bitSet(PINB, led);
-  delay(sleep);
+  digitalWrite(led, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(sleep);            // wait for a second
+  digitalWrite(led, LOW);  // turn the LED off by making the voltage LOW
+  delay(sleep);            // wait for a second
 }
